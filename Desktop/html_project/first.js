17ade16eaa88a45568f11e14c83bd86e8832a944
Laxman <pachelaxman22@gmail.com>
2020-12-14 13:53:25 +0530

    web based clock is created

---
+++ Desktop/html_project/first.js
@@ -0,0 +1,33 @@
+//ways to print in javascript
+//console.log("Hello word");
+//alert("hello")
+// document.write("THis is socument write")
+
+// Javascript console API
+//console.log("HI");
+//console.warn("This is warning");
+//console.error("this is error")
+
+// javascript variable
+/*var num1 = 10;
+ var num2 = 10;
+document.write(num1 + num2);
+*/
+
+//Data types in javascript
+var num1 = 24;
+var str1 = "hi i am string";
+
+//object
+var marks = {
+
+    ravi: 34,
+    shubhame: 78,
+    harry: 34
+}
+console.log(marks['ravi'])
+
+//booleans
+var a = true
+var b = true
+console.log(a)
